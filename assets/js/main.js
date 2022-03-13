00035a9ceea493336dcd1a8e39e0726a18dd09c9
erfanmoshafi <erfan.moshafi2018@gmail.com>
2022-03-13 08:34:56 +0330

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,17 @@
-window.onload= function(){
+window.onscroll= function(){
     const divs=['about_product'];
-   alert(document.getElementById(divs[0]).offsetHeight)
+// //    alert(document.getElementById(divs[0]).getBoundingClientRect().top )
+// // divs.forEach(function(item){
+// //  alert(document.getElementById(divs[0]).offsetTop);
+// // if(window.scrollY==document.getElementById(divs[0]).offsetTop){
+// //     alert('ok')
+// // }
+// var div =document.getElementById(divs[0]).scrollHeight
+// console.log('window='+window.scrollY)
+// console.log('div='+div)
+// // });
+// if(window.scrollY>=document.getElementById(divs[0]).offsetTop&&window.scrollY<=document.getElementById(divs[0]).offsetbottom) {
+//     console.log('ok');
+// }
+// console.log(window.scrollY)
 }
