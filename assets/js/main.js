33b8b84003cf22a78c8e464be19055ce7d623959
erfanmoshafi <erfan.moshafi2018@gmail.com>
2022-03-16 23:11:20 +0330

    update header menu active

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,22 @@
 window.onscroll = function () {
     const divs = ['about_product'];
-   divs.forEach(function(item){
-    var j, i;
-    for (i = 0, j = 20; i < 20; i++, j--) {
-        var scroll_bottom=document.getElementById(item).offsetTop+document.getElementById(item).offsetHeight;
-        if ((window.scrollY == (document.getElementById(item).offsetTop + i) || window.scrollY == (document.getElementById(item).offsetTop - j))|| (window.scrollY ==(scroll_bottom + i) || window.scrollY == (scroll_bottom - j))) {
-            console.log('ok'); 
+    divs.forEach(function (item) {
+        var j, i;
+        for (i = 0, j = 20; i < 20; i++, j--) {
+            var element = document.getElementById(item);
+            var element_line = document.getElementById(item + '-line-menu');
+            var scroll_bottom = element.offsetTop + element.offsetHeight;
+            if ((window.scrollY == (element.offsetTop + i) || window.scrollY == (element.offsetTop - j)) || (window.scrollY == (scroll_bottom + i) || window.scrollY == (scroll_bottom - j))) {
+                if (element_line.classList.contains('active')===false) {
+                    element_line.classList.add('active')
+                }
+            }else{
+                if (element_line.classList.contains('active')) {
+                    element_line.classList.remove('active')
+                } 
+            }
         }
-    }
-   })
+    })
 }
 function open_menu_mobile_menu() {
     if (document.querySelector('.header-menu-list').classList.contains('active')) {
