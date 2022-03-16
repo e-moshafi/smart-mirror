f034f3ea438b004325ab19524a0b125d1d146ac7
erfanmoshafi <erfan.moshafi2018@gmail.com>
2022-03-16 22:58:37 +0330

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,34 @@
-window.onscroll= function(){
-    const divs=['about_product'];
-// //    alert(document.getElementById(divs[0]).getBoundingClientRect().top )
-// // divs.forEach(function(item){
-// //  alert(document.getElementById(divs[0]).offsetTop);
-// // if(window.scrollY==document.getElementById(divs[0]).offsetTop){
-// //     alert('ok')
-// // }
-// var div =document.getElementById(divs[0]).scrollHeight
-// console.log('window='+window.scrollY)
-// console.log('div='+div)
-// // });
-// if(window.scrollY>=document.getElementById(divs[0]).offsetTop&&window.scrollY<=document.getElementById(divs[0]).offsetbottom) {
-//     console.log('ok');
-// }
-// console.log(window.scrollY)
+window.onscroll = function () {
+    const divs = ['about_product'];
+   divs.forEach(function(item){
+    var j, i;
+    for (i = 0, j = 20; i < 20; i++, j--) {
+        var scroll_bottom=document.getElementById(item).offsetTop+document.getElementById(item).offsetHeight;
+        if ((window.scrollY == (document.getElementById(item).offsetTop + i) || window.scrollY == (document.getElementById(item).offsetTop - j))|| (window.scrollY ==(scroll_bottom + i) || window.scrollY == (scroll_bottom - j))) {
+            console.log('ok');
+        }
+    }
+   })
 }
 function open_menu_mobile_menu() {
-    if(document.querySelector('.header-menu-list').classList.contains('active')){
+    if (document.querySelector('.header-menu-list').classList.contains('active')) {
         document.querySelector('.header-menu-list').classList.remove('active');
         document.querySelector('.bars-menu-icon').classList.remove('fa-times')
         document.querySelector('.bars-menu-icon').classList.add('fa-bars')
-    }else{
+    } else {
         document.querySelector('.header-menu-list').classList.add('active')
         document.querySelector('.bars-menu-icon').classList.add('fa-times')
         document.querySelector('.bars-menu-icon').classList.remove('fa-bars')
     }
 }
-function icon_hidden_form(value_input,name_element){
-    var element = document.querySelector('.rounded-icon-'+name_element);
-    if(value_input===''||value_input===' '){
-        if(element.classList.contains('deactive-element-show')===true){
+function icon_hidden_form(value_input, name_element) {
+    var element = document.querySelector('.rounded-icon-' + name_element);
+    if (value_input === '' || value_input === ' ') {
+        if (element.classList.contains('deactive-element-show') === true) {
             element.classList.remove('deactive-element-show');
         }
-    }else{
-        if(element.classList.contains('deactive-element-show')===false){
+    } else {
+        if (element.classList.contains('deactive-element-show') === false) {
             element.classList.add('deactive-element-show');
         }
 
